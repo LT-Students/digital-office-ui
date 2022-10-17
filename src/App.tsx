9a13ed9eacf9e9cfe4b68989bc01d62536e3114f
Azaro <qppazaro@gmail.com>
2022-10-18 00:46:32 +0800

    Added MUI lib. Done initial configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MuiButton from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
-function App() {
+import { Button } from "components/ui";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack
+      spacing={2}
+      direction="row"
+      sx={{
+        mt: 2,
+        justifyContent: "center",
+      }}
+    >
+      <MuiButton>Default Button</MuiButton>
+      <Button>Custom Button</Button>
+    </Stack>
   );
-}
+};
 
 export default App;
